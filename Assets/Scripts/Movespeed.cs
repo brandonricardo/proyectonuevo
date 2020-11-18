421a1c344e2afd08703cab704f870536ae81bf40
brandon <C18460018@colima.tecnm.mx>
2020-11-18 02:35:50 -0600

    proyecto
    https://github.com/JCarlosOli1/Proyecto_nuevo.git

---
+++ Assets/Scripts/Movespeed.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movespeed : MonoBehaviour
+{
+    public float runSpeed = 2;
+    public float jumpSpeed = 5;
+    // Start is called before the first frame update
+    Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+        }
+        if (Input.GetKey("space") && CheckGround.isGrounded)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+        }
+    }
+}
